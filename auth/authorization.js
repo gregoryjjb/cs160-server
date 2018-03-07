c640fe87f0d27fc5d2a0b22416c5c9f61c35954d
Greg <gregoryjjb@gmail.com>
2018-03-07 15:37:21 -0800

    Modified authorization function to check cookies instaed of auth header

--- auth/authorization.js
+++ auth/authorization.js
@@ -2,25 +2,30 @@ var models = require('../models');
 
 const authorization = (req, res, next) => {
     
-    var sessionId = req.headers.authorization;
+    var sessionId = req.cookies.sessionId; //req.headers.authorization;
 
-    models.User.findOne({where: {
-        sessionId: sessionId
-    }})
-    .then(user => {
-        if(user) {
-            res.locals.userId = user.id;
-            next();
-        }
-        else {
+    if(sessionId) {
+        models.User.findOne({where: {
+            sessionId: sessionId
+        }})
+        .then(user => {
+            if(user) {
+                res.locals.userId = user.id;
+                next();
+            }
+            else {
+                res.status(401).end('Access denied');
+            }
+
+            return null; // To avoid unreturned promise
+        })
+        .catch(error => {
             res.status(401).end('Access denied');
-        }
-
-        return null; // To avoid unreturned promise
-    })
-    .catch(error => {
-        res.status(401).send('Access denied');
-    });
+        });
+    }
+    else {
+        res.status(401).end('Access denied');
+    }
 }
 
 module.exports = authorization;
