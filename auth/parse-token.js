4b902f9e50e815b27b5c5a6d0bf6a2f173903bf2
Greg Brisebois <Gregory.Brisebois@analog.com>
2018-03-23 12:07:36 -0700

    Fix: audiAnce -> audiEnce

--- auth/parse-token.js
+++ auth/parse-token.js
@@ -1,12 +1,10 @@
 const { OAuth2Client } = require('google-auth-library');
-const audiance = "15965114724-3b71qboka3ij341kddr78df42et1nt7q.apps.googleusercontent.com";
-var gapiClient = new OAuth2Client(audiance, '', '');
+const audience = "15965114724-3b71qboka3ij341kddr78df42et1nt7q.apps.googleusercontent.com";
+var gapiClient = new OAuth2Client(audience, '', '');
 
 var parser = (idToken, onSuccess, onFailure) => {
-    const audiance2 = "15965114724-3b71qboka3ij341kddr78df42et1nt7q.apps.googleusercontent.com";
-    gapiClient.verifyIdToken({idToken, audiance2})
+    gapiClient.verifyIdToken({idToken, audience})
     .then(login => {
-        //console.log(login.getPayload());
         onSuccess(login.getPayload());
     })
     .catch(error => {
