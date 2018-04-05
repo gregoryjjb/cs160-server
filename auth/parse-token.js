9c02b00326644247e28f307fb6a32231cffde132
Greg <gregoryjjb@gmail.com>
2018-04-04 17:26:23 -0700

    Forced Google Sign-in to fail if no client ID is specified

--- auth/parse-token.js
+++ auth/parse-token.js
@@ -2,22 +2,29 @@ const { OAuth2Client } = require('google-auth-library');
 const audience = process.env.GOOGLE_CLIENT_ID;
 var gapiClient = new OAuth2Client(audience, '', '');
 
+// Give alert if audience is not set, as Google sign-in
+// will fail
 if(audience) {
     console.log("Google Sign-in Initialized");
     console.log("\tClient ID:", audience);
 }
 else {
-    console.error("ERROR: No Google Client ID set, sign-in is insecure. Set GOOGLE_CLIENT_ID");
+    console.error("ERROR: No Google Client ID set, sign-in WILL FAIL. Set env variable GOOGLE_CLIENT_ID");
 }
 
 var parser = (idToken, onSuccess, onFailure) => {
-    gapiClient.verifyIdToken({idToken, audience})
-    .then(login => {
-        onSuccess(login.getPayload());
-    })
-    .catch(error => {
-        if(onFailure) onFailure(error);
-    });
+    if(audience) {
+        gapiClient.verifyIdToken({idToken, audience})
+        .then(login => {
+            onSuccess(login.getPayload());
+        })
+        .catch(error => {
+            if(onFailure) onFailure(error);
+        });
+    }
+    else {
+        onFailure({err: "No audience specified"});
+    }
 }
 
 module.exports = parser;
