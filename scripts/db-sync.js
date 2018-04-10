c6bbfefb25e8be57daf2eaa170b22e586dd3ddb5
Greg Brisebois <gregoryjjb@gmail.com>
2018-04-09 20:56:02 -0700

    Deletes video files on sync

--- scripts/db-sync.js
+++ scripts/db-sync.js
@@ -1,4 +1,20 @@
-var models = require('../models');
+const models = require('../models');
+const fs = require('fs');
+const path = require('path');
+
+console.log("Deleting video files..");
+fs.readdir('./videos', (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+        if(file !== '.gitkeep') {
+            fs.unlink(path.join('./videos', file), err => {
+                if (err) throw err;
+            });
+        }
+    }
+});
+console.log("Files deleted");
 
 console.log('Synchronizing models to database...');
 console.log('***********************************');
