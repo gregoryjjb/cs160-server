991f6f9d2d07d5ef33baa26aea9c8c808fd63379
Greg Brisebois <gregoryjjb@gmail.com>
2018-04-15 16:40:10 -0700

    Made streaming and processing with RTSP work

--- utils/stream-socket.js
+++ utils/stream-socket.js
@@ -8,6 +8,8 @@ module.exports = function(server) {
     const io = require('socket.io')(server);
     
     var ffmpeg = null;
+    var cv = null;
+    var ffmpeg2 = null;
 
     io.on('connection', socket => {
         console.log('User connected to socket');
@@ -18,6 +20,14 @@ module.exports = function(server) {
                 console.log('Killing FFMPEG');
                 ffmpeg.kill();
             }
+            if(cv && cv.kill) {
+                console.log('Killing CVProcessor');
+                cv.kill();
+            }
+            if(ffmpeg2 && ffmpeg2.kill) {
+                console.log('Killing FFMPEG2');
+                ffmpeg2.kill();
+            }
         })
 
         ss(socket).on('vid', stream => {
@@ -26,7 +36,7 @@ module.exports = function(server) {
             
             var timestamp = new Date().getTime();
             
-            var args = ['-re', '-i', 'pipe:0', '-f', 'rtsp', `rtsp://localhost:5545/foo.mp4`];
+            var args = ['-re', '-i', 'pipe:0', '-f', 'rtsp', '-muxdelay', '0.1', `rtsp://localhost:5545/foo.mp4`];
             ffmpeg = child_process.spawn('ffmpeg', args);
             //ffmpeg.stdout.on('data', (data) => {console.log(data)});
             //ffmpeg.stderr.on('data', (data) => {console.log(data)});
@@ -34,11 +44,26 @@ module.exports = function(server) {
             ffmpeg.stderr.pipe(process.stderr);
             stream.pipe(ffmpeg.stdin);
             ffmpeg.stdin.on('error', err => {
-                console.log("############################################# ERROR");
-                console.log(err);
+                console.log("! FFMPEG stdin error: write pipe was probably closed");
             })
             
-            var cvArgs = ['-s', 'rtsp://localhost:5545/node.mp4'];
+            setTimeout(() => {
+                var cvArgs = ['-s', 'rtsp://localhost:554/foo.mp4'];
+                cv = child_process.spawn('./cvprocessor', cvArgs, {cwd: './processing/cs160/CVProcessor/dist/Debug/GNU-Linux/'});
+                cv.stdout.pipe(process.stdout);
+                cv.stderr.pipe(process.stderr);
+                cv.on('close', (code, signal) => console.log('CVProcessor exited with code', code, 'signal', signal));
+            }, 6000);
+            
+            setTimeout(() => {
+                var fbackArgs = ['-i', 'rtsp://localhost:554/processed.mp4', '-f', 'webm', '-vcodec', 'vp8', 'pipe:1'];
+                ffmpeg2 = child_process.spawn('ffmpeg', fbackArgs);
+                
+                var backStream = ss.createStream();
+                ffmpeg2.stdout.pipe(backStream);
+                ss(socket).emit('vid-back', backStream);
+                
+            }, 12000 );
             
             // THIS IS FOR THE OLD WAY, USING STDIN AND OUT
             
