1938be20f3d074cc33cd5f392c9d7dc4407de0e3
Greg Brisebois <gregoryjjb@gmail.com>
2018-04-16 23:06:21 +0000

    Added support for postgresql

--- config/config.js
+++ config/config.js
@@ -21,6 +21,7 @@ module.exports = {
 	},
     production: {
         db: {
+            dialect: process.env.DB_DIALECT,
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
