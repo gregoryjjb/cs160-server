da8bc27b701a5f64d1dad4deddd1f69c4131fcd5
Greg Brisebois <gregoryjjb@gmail.com>
2018-04-15 18:59:21 -0700

    Cleaned up streaming

--- utils/processing.js
+++ utils/processing.js
@@ -1,10 +1,14 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const child_process = require('child_process');
+const ss = require('socket.io-stream');
 const fs = require('fs');
 
 const config = {
-    executable: 'cvprocessor',
+    executable: './cvprocessor',
     cwd: './processing/cs160/CVProcessor/dist/Release/GNU-Linux/',
+    rtspTarget: 'rtsp://localhost:5545',
+    rtspSource: 'rtsp://localhost:554',
 }
 
 // Timestamp
@@ -40,5 +44,84 @@ processing.processFile = async function(file, filename) {
     
     return stdout;
 }
+
+function Stream(stream, callback) {
+    // Arguments
+    this.streamToRtspArgs = ['-re', '-i', 'pipe:0', '-f', 'rtsp', '-muxdelay', '0.1', `rtsp://localhost:5545/foo.mp4`];
+    this.cvArgs = ['-s', `rtsp://localhost:554/foo.mp4`];
+    this.rtspToStreamArgs = ['-i', `${config.rtspSource}/processed.mp4`, '-f', 'webm', '-vcodec', 'vp8', 'pipe:1'];
+    
+    // Child processes
+    this.ffmpeg;
+    this.cv;
+    this.ffmpeg2;
+    
+    // Timeouts
+    this.cvTO;
+    this.ffmpegTO;
+    
+    // First FFMPEG
+    this.ffmpeg = child_process.spawn('ffmpeg', this.streamToRtspArgs);
+    stream.pipe(this.ffmpeg.stdin);
+    this.ffmpeg.stdin.on('error', err => {
+        console.log("FFMPEG stdin error: write pipe was probably closed");
+        this.kill();
+    });
+    this.ffmpeg.on('close', code => console.log("FFMPEG exited with code", code));
+    // Log
+    if(true) {
+        this.ffmpeg.stdout.pipe(process.stdout);
+        this.ffmpeg.stderr.pipe(process.stderr);
+    }
+    
+    // Run processing on RTSP
+    this.cvTO = setTimeout(() => {
+        this.cv = child_process.spawn(config.executable, this.cvArgs, {cwd: config.cwd});
+        this.cv.on('close', (code, signal) => console.log('CVProcessor exited with code', code, 'signal', signal));
+        // Log
+        if(true) {
+            this.cv.stdout.pipe(process.stdout);
+            this.cv.stderr.pipe(process.stderr);
+        }
+    }, 4000 );
+    
+    // Turn RTSP back into node stream
+    this.ffmpegTO = setTimeout(() => {
+        this.ffmpeg2 = child_process.spawn('ffmpeg', this.rtspToStreamArgs);
+        var backStream = ss.createStream();
+        this.ffmpeg2.stdout.pipe(backStream);
+        callback(backStream);
+    }, 8000 );
+}
+
+Stream.prototype.kill = function() {
+    
+    if(this.ffmpeg && this.ffmpeg.kill) {
+        console.log('Killing FFMPEG');
+        this.ffmpeg.kill();
+    }
+    else {
+        console.log('FFMPEG not started so not killed');
+    }
+    if(this.cv && this.cv.kill) {
+        console.log('Killing CVProcessor');
+        this.cv.kill();
+    }
+    else {
+        console.log('CVProcessor not started so not killed');
+    }
+    if(this.ffmpeg2 && this.ffmpeg2.kill) {
+        console.log('Killing FFMPEG2');
+        this.ffmpeg2.kill();
+    }
+    else {
+        console.log('FFMPEG2 not started so not killed');
+    }
+    
+    clearTimeout(this.cvTO);
+    clearTimeout(this.ffmpegTO);
+}
+
+processing.Stream = Stream;
 
 module.exports = processing;
--- utils/stream-socket.js
+++ utils/stream-socket.js
@@ -4,30 +4,20 @@ var ss = require('socket.io-stream');
 var fs = require('fs');
 var devnull = require('dev-null');
 
+const processing = require('./processing');
+
 module.exports = function(server) {
     const io = require('socket.io')(server);
-    
-    var ffmpeg = null;
-    var cv = null;
-    var ffmpeg2 = null;
 
     io.on('connection', socket => {
         console.log('User connected to socket');
+        
+        var streamProcess;
 
         socket.on('disconnect', () => {
             console.log('User disconnected');
-            if(ffmpeg && ffmpeg.kill) {
-                console.log('Killing FFMPEG');
-                ffmpeg.kill();
-            }
-            if(cv && cv.kill) {
-                console.log('Killing CVProcessor');
-                cv.kill();
-            }
-            if(ffmpeg2 && ffmpeg2.kill) {
-                console.log('Killing FFMPEG2');
-                ffmpeg2.kill();
-            }
+            
+            if(streamProcess) streamProcess.kill();
         })
 
         ss(socket).on('vid', stream => {
@@ -36,7 +26,11 @@ module.exports = function(server) {
             
             var timestamp = new Date().getTime();
             
-            var args = ['-re', '-i', 'pipe:0', '-f', 'rtsp', '-muxdelay', '0.1', `rtsp://localhost:5545/foo.mp4`];
+            streamProcess = new processing.Stream(stream, backStream => {
+                ss(socket).emit('vid-back', backStream);
+            })
+            
+            /*var args = ['-re', '-i', 'pipe:0', '-f', 'rtsp', '-muxdelay', '0.1', `rtsp://localhost:5545/foo.mp4`];
             ffmpeg = child_process.spawn('ffmpeg', args);
             //ffmpeg.stdout.on('data', (data) => {console.log(data)});
             //ffmpeg.stderr.on('data', (data) => {console.log(data)});
@@ -53,7 +47,7 @@ module.exports = function(server) {
                 cv.stdout.pipe(process.stdout);
                 cv.stderr.pipe(process.stderr);
                 cv.on('close', (code, signal) => console.log('CVProcessor exited with code', code, 'signal', signal));
-            }, 6000);
+            }, 6000 );
             
             setTimeout(() => {
                 var fbackArgs = ['-i', 'rtsp://localhost:554/processed.mp4', '-f', 'webm', '-vcodec', 'vp8', 'pipe:1'];
@@ -63,7 +57,7 @@ module.exports = function(server) {
                 ffmpeg2.stdout.pipe(backStream);
                 ss(socket).emit('vid-back', backStream);
                 
-            }, 12000 );
+            }, 12000 );*/
             
             // THIS IS FOR THE OLD WAY, USING STDIN AND OUT
             
@@ -83,10 +77,6 @@ module.exports = function(server) {
             /*var backStream = ss.createStream();
             ffmpeg.stdout.pipe(backStream);
             ss(socket).emit('vid-back', backStream);*/
-            
-            ffmpeg.on('close', code => {
-                console.log("FFMPEG exited with code", code);
-            })
             
             stream.on('end', () => {
                 console.log('Stream ended');
