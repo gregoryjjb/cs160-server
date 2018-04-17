db102acf1d42d606cb7aec463b68fbf76e7d81b6
Greg Brisebois <gregoryjjb@gmail.com>
2018-04-16 18:40:06 -0700

    Cleaned up unused functionality related to processing

--- routes/videos.js
+++ routes/videos.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var multer = require('multer');
-var upload = multer({dest: 'videos/'});
+//var multer = require('multer');
+//var upload = multer({dest: 'videos/'});
 var fileUpload = require('express-fileupload');
 var models = require('../models');
 var authorization = require('../auth/authorization');
 
-const util = require('util');
+//const util = require('util');
 const child_process = require('child_process');
-const exec = util.promisify(require('child_process').exec);
+//const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
 
 const processing = require('../utils/processing');
@@ -30,26 +30,6 @@ router.route('/')
 	let video = await models.Video.create({ name, userId: res.locals.userId });
 	
 	const filename = video.id + '_' + file.name;
-	/*const tempFilename = 'TEMP_' + filename;
-
-	const finalPath = './videos/' + filename;
-	const tempPath = './videos/' + tempFilename;
-
-	console.log("Processing job requested for", filename);
-	
-	console.log("\tMoving file", tempPath);
-	file.mv(tempPath);
-	console.log("\tFinished move");
-
-	const cvTempPath = __dirname + '/../videos/' + tempFilename;
-	const cvFinalPath = __dirname + '/../videos/' + filename;
-	
-	console.log("\tStart processing");
-	//const { stdout, stderr } = await exec(command, {cwd: './processing/cs160/CVProcessor/dist/Release/GNU-Linux/', maxBuffer: 1024 * 10000});
-	const { stdout, stderr } = await processing.processFile(cvTempPath, cvFinalPath);
-	console.log("\tEnd processing");
-	
-	//fs.writeFile('./output.txt', stdout, (err) => {});*/
 	
 	let processingData = await processing.processFile(file, filename);
 	
@@ -58,23 +38,10 @@ router.route('/')
 	res.send({});
 })
 
-router.route('/stream')
-.get((req, res) => {
-	var args = ['-re', '-i', '/home/greg/Videos/problem.mp4', '-f', 'rtsp', '-muxdelay', '0.1', `rtsp://localhost:5545/node.sdp`];
-	var ffmpeg = child_process.spawn('ffmpeg', args);
-	ffmpeg.stdout.on('data', (data) => {console.log(data)});
-	ffmpeg.stderr.on('data', (data) => {console.log(data)});
-	//stream.pipe(ffmpeg.stdin);
-	ffmpeg.stdin.on('error', err => {
-		console.log("############################################# ERROR");
-		console.log(err);
-	})
-})
-
 router.route('/:userId')
 .get(async (req, res) => {
     
-    const { userId } = res.locals; //req.params.userId;
+    const { userId } = res.locals;
     
     const videos = await models.Video.findAll({
         where: { userId }
--- utils/stream-socket.js
+++ utils/stream-socket.js
@@ -15,13 +15,13 @@ module.exports = function(server) {
         var streamProcess;
 
         socket.on('disconnect', () => {
-            console.log('User disconnected');
+            console.log('User disconnected from socket');
             
             if(streamProcess) streamProcess.kill();
         })
 
         ss(socket).on('vid', stream => {
-            console.log('Got a stream!');
+            console.log('Received a live stream');
             stream.pipe(devnull());
             
             var timestamp = new Date().getTime();
@@ -29,54 +29,6 @@ module.exports = function(server) {
             streamProcess = new processing.Stream(stream, backStream => {
                 ss(socket).emit('vid-back', backStream);
             })
-            
-            /*var args = ['-re', '-i', 'pipe:0', '-f', 'rtsp', '-muxdelay', '0.1', `rtsp://localhost:5545/foo.mp4`];
-            ffmpeg = child_process.spawn('ffmpeg', args);
-            //ffmpeg.stdout.on('data', (data) => {console.log(data)});
-            //ffmpeg.stderr.on('data', (data) => {console.log(data)});
-            ffmpeg.stdout.pipe(process.stdout);
-            ffmpeg.stderr.pipe(process.stderr);
-            stream.pipe(ffmpeg.stdin);
-            ffmpeg.stdin.on('error', err => {
-                console.log("! FFMPEG stdin error: write pipe was probably closed");
-            })
-            
-            setTimeout(() => {
-                var cvArgs = ['-s', 'rtsp://localhost:554/foo.mp4'];
-                cv = child_process.spawn('./cvprocessor', cvArgs, {cwd: './processing/cs160/CVProcessor/dist/Debug/GNU-Linux/'});
-                cv.stdout.pipe(process.stdout);
-                cv.stderr.pipe(process.stderr);
-                cv.on('close', (code, signal) => console.log('CVProcessor exited with code', code, 'signal', signal));
-            }, 6000 );
-            
-            setTimeout(() => {
-                var fbackArgs = ['-i', 'rtsp://localhost:554/processed.mp4', '-f', 'webm', '-vcodec', 'vp8', 'pipe:1'];
-                ffmpeg2 = child_process.spawn('ffmpeg', fbackArgs);
-                
-                var backStream = ss.createStream();
-                ffmpeg2.stdout.pipe(backStream);
-                ss(socket).emit('vid-back', backStream);
-                
-            }, 12000 );*/
-            
-            // THIS IS FOR THE OLD WAY, USING STDIN AND OUT
-            
-            // Send stream to ffmpeg
-            /*var args = ['-i', 'pipe:0', '-f', 'webm', '-vcodec', 'vp8', `pipe:1`];
-            var ffmpeg = child_process.spawn('ffmpeg', args);
-            ffmpeg.stdout.on('data', (data) => {console.log(data)});
-            ffmpeg.stderr.on('data', (data) => {console.log(data)});
-            stream.pipe(ffmpeg.stdin);
-            
-            ffmpeg.stdin.on('error', err => {
-                console.log("############################################# ERROR");
-                console.log(err);
-            })*/
-            
-            // Send stream back to client
-            /*var backStream = ss.createStream();
-            ffmpeg.stdout.pipe(backStream);
-            ss(socket).emit('vid-back', backStream);*/
             
             stream.on('end', () => {
                 console.log('Stream ended');
