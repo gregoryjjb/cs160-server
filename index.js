5a3534efc67fb8df773d68eb23204cc5a8c4b7a7
Greg <gregoryjjb@gmail.com>
2018-04-18 15:01:03 -0700

    Added warning if CVProcessor is not present

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config();
 dotenv.config({path: '.env.local'});
 
 var http = require('http');
+var fs = require('fs');
 var streamSocket = require('./utils/stream-socket');
 var models = require('./models');
 var app = require('./app');
@@ -16,8 +17,17 @@ var port =  process.env.PORT || 4000;
 var env = process.env.NODE_ENV || 'development';
 
 ///////////////////////////////////////////////////////
-// Do socket stuff
+// Check for existance of cvprocessor
 
+console.log("Searching for CVProcessor...");
+fs.exists('./processing/cs160/CVProcessor/dist/Release/GNU-Linux/', exists => {
+    if(exists) {
+        console.log("\tCVProcessor found");
+    }
+    else {
+        console.log("\tCVProcessor not found; clone the cs-160 repo into processing/");
+    }
+})
 
 ///////////////////////////////////////////////////////
 
