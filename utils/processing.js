a913277433968fa8ae0e098484275e42a9fcf52f
Greg Brisebois <gregoryjjb@gmail.com>
2018-04-27 17:47:43 -0700

    Piped video to cvprocessor stdin

--- utils/processing.js
+++ utils/processing.js
@@ -1,5 +1,6 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const execSync = require('child_process').execSync;
 const child_process = require('child_process');
 const ss = require('socket.io-stream');
 const fs = require('fs');
@@ -60,23 +61,83 @@ function Stream(stream, callback) {
     console.log('\t' + this.rawStreamName);
     console.log('\t' + this.streamName);
     
+    stream.pipe(devnull());
+    
     // Arguments
-    this.streamToRtspArgs = ['-re', '-i', 'pipe:0', '-f', 'rtsp', '-muxdelay', '0.1', `${config.rtspTarget}/${this.rawStreamName}`];
+    this.ffprobeArgs = [
+        '-v', 'error',
+        '-select_streams', 'v:0',
+        '-show_entries', 'stream=avg_frame_rate,,width,height',
+        '-of', 'default=noprint_wrappers=1:nokey=1',
+        'pipe:0'
+    ];
+    
+    //this.streamToRtspArgs = ['-re', '-i', 'pipe:0', '-f', 'rtsp', '-muxdelay', '0.1', `${config.rtspTarget}/${this.rawStreamName}`];
     this.cvArgs = [
-        '-s', `${config.rtspSource}/${this.rawStreamName}`,
-        '-o', `pipe:1`,
+        '-stdio',
+        '24/1',
+        '640',
+        '480',
         '-of', '-f webm -c:v vp8 -b:v .5M -g 1',
     ];
     
     // Child processes
+    this.ffprobe;
     this.ffmpeg;
     this.cv;
     
     // Timeouts
     this.cvTO;
     
+    // Run FFProbe
+    console.log("Running FFProbe");
+    this.ffprobe = child_process.spawn('ffprobe', this.ffprobeArgs);
+    var ffprobeStdout = "";
+    
+    // Get FFProbe STDOUT
+    this.ffprobe.stdout.on('data', data => {
+        ffprobeStdout += data.toString();
+    })
+    
+    // Handle FFProbe STDIN close
+    this.ffprobe.stdin.on('error', err => {
+        console.log("FFProbe STDIN closed");
+    })
+    
+    
+    
+    this.ffmpeg = child_process.spawn('ffmpeg', ['-i', 'pipe:0', '-f', 'webm', '-c:v', 'vp8', '-b:v', '.5M', '-g', '1', 'pipe:1']);
+    stream.pipe(this.ffmpeg.stdin);
+    this.ffmpeg.stdout.pipe(devnull());
+    this.ffmpeg.stderr.pipe(process.stdout);
+    
+    this.ffmpeg.stdout.pipe(this.ffprobe.stdin);
+    
+    setTimeout(() => {
+    // On FFProbe finished
+    //this.ffprobe.on('close', code => {
+        //console.log('FFProbe exited with code', code);
+        //console.log(ffprobeStdout.replace('\n', 'NEWLINE'));
+        
+        console.log('Starting CVProcessor');
+        
+        // Start CVProcessor
+        this.cv = child_process.spawn(config.executable, this.cvArgs, {cwd: config.cwd});
+        this.cv.on('close', (code, signal) => console.log('CVProcessor exited with code', code, 'signal', signal));
+        this.ffmpeg.stdout.pipe(this.cv.stdin);
+        
+        this.cv.stderr.pipe(devnull());
+        this.cv.stdout.pipe(devnull());
+        
+        var backStream = ss.createStream();
+        this.cv.stdout.pipe(backStream);
+        callback(backStream)
+    //})
+    }, 5000);
+    
+    
     // First FFMPEG
-    this.ffmpeg = child_process.spawn('ffmpeg', this.streamToRtspArgs);
+    /*this.ffmpeg = child_process.spawn('ffmpeg', this.streamToRtspArgs);
     stream.pipe(this.ffmpeg.stdin);
     this.ffmpeg.stdin.on('error', err => {
         console.log("FFMPEG stdin error: write pipe was probably closed");
@@ -105,7 +166,7 @@ function Stream(stream, callback) {
             //this.cv.stdout.pipe(process.stdout);
             this.cv.stderr.pipe(process.stderr);
         //}
-    }, 6000 );
+    }, 6000 );*/
 }
 
 Stream.prototype.kill = function() {
